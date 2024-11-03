fbd7f959e88da6df99c64dd57b8bea5f850228a0
farrisst <farrisst@protonmail.com>
2024-11-03 16:21:04 -0500

    Game Shop Work

--- main.py
+++ main.py
@@ -1,16 +1,38 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
+    dt = 0  
+
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    player.Containers = (updateable, drawable)
     
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
              return
-        screen.fill(COLOR)
+            
+        screen.fill(BLACK)
+        
+        for i in drawable:
+            i.draw(screen)
+
+        for i in updateable:
+           i.update(dt)
+
+
+        player.draw(screen)
+        player.update(dt)
+
         pygame.display.flip()
+        dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
